ae3c67adebbcc1679b2d43bfab3121fd7d2cbb14
Fred Vuni <fredvuni809@gmail.com>
2023-11-20 13:32:29 +0300

    chore: set up home route

--- website/website/urls.py
+++ website/website/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("music/",include("music.urls")),
+    path("",include("music.urls")),
 ]
 
 if settings.DEBUG:
